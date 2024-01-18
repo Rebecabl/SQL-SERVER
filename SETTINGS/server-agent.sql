1929bd02f1f634d5f0e05c96fb55676f6da453ed
Rebeca Barbosa <107273033+Rebecabl@users.noreply.github.com>
2024-01-18 13:32:32 -0300

    SETTINGS

---
+++ SETTINGS/server-agent.sql
@@ -0,0 +1,5 @@
+---- O SQL Server Agent é um componente do Microsoft SQL Server que funciona como um serviço do Windows e permite a automação e agendamento de tarefas administrativas no SQL Server. 
+---- Ele oferece um conjunto de recursos que facilitam o gerenciamento e a execução de tarefas automatizadas, como backups, otimização de índices, execução de scripts T-SQL e notificação de eventos.
+
+BACKUP DATABASE SeuBancoDeDados TO DISK = 'C:\Caminho\Para\O\Backup\BackupCompleto.bak' WITH INIT;
+
